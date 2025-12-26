06146b4ee3332355e3c11173128107d02932bd7c
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2025-12-26 10:05:57 +0300

    Add result to assertThrows

--- test/Test/Utils.hs
+++ test/Test/Utils.hs
@@ -12,9 +12,9 @@ instance Show (a -> b) where
 instance (Enum a, Bounded a, Eq b) => Eq (a -> b) where
   xs == ys = map xs domain == map ys domain
 
-assertThrows :: forall exception a . Exception exception => a -> IO ()
+assertThrows :: forall exception a . (Show a, Exception exception) => a -> IO ()
 assertThrows comp = try (evaluate comp) >>= \case
   Left e -> case fromException @exception e of
     Just _ -> pure ()
     Nothing -> throw e
-  Right _ -> assertFailure "Expected exception, got result"
+  Right result -> assertFailure $ "Expected exception, got result: " <> show result
